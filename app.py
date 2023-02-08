f3a9672b6ad8d3b1eb87eddf3a24750cc2966459
turonov <turonov5400gmail.com>
2023-02-08 10:25:50 +0500

    api

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def hello_word():
+    return "Hello Word"
 @app.route('/home')
 def home():
-    return 'Home Page!!'
+    return "home page"
 
-# This will run the app on http://localhost:5000
 if __name__ == '__main__':
-    # Run the app in local network
-    app.run(host='0.0.0.0', port=8000,debug=True)
+    app.run()
