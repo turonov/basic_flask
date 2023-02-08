efd73489b32a1c80624667ab4db98b9aaba2e5bc
turonov <turonov5400gmail.com>
2023-02-08 11:05:47 +0500

    init

--- app.py
+++ app.py
@@ -5,9 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_word():
     return "Hello Word"
+
 @app.route('/home')
 def home():
-    return "home page"
+    return "Home page"
+
+@app.route('/index')
+def index():
+    return "Index page"
+
+@app.route('/page')
+def page():
+    return "page"
 
 if __name__ == '__main__':
     app.run()
