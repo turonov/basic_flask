2043b234a313fe5e01d167b2dcf72d24f9dc3db2
turonov <turonov5400gmail.com>
2023-02-10 10:55:02 +0500

    qaytish

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_word():
-    return "Hello Word"
+    r=request.args
+    name=r['name']
+    data = {
+        "apple":14,
+        "banana":1,
+        "orange":4,
+        "grape":10
+    }
+    return {name:data.get(name,'Not Found')}
 
 @app.route('/home')
 def home():
